28d0ae1505f0ab3c2412e1f79079b8b09ca0d838
nabeelCpp <nabeel_arshad93@outlook.com>
2023-10-22 11:08:42 +0500

    Booking form in agent dashboard is completed

--- src/pages/agent-dashboard/index.js
+++ src/pages/agent-dashboard/index.js
@@ -1,20 +1,79 @@
 // ** MUI Imports
-import Grid from '@mui/material/Grid'
+import { Grid, Card } from '@mui/material'
+import { useState } from 'react'
 import { useAuth } from 'src/hooks/useAuth'
 import Congratulations from 'src/views/agentDashboard/Congratulations'
-import HorizontalForm from 'src/views/pages/agent/bookingForm'
+import { airports } from 'src/views/pages/agent/bookingForm/airports'
+import HorizontalForm from 'src/views/pages/agent/bookingForm/from'
+
+const convertMinutesToHoursAndMinutes = minutes => {
+  const hours = Math.floor(minutes / 60)
+  const remainingMinutes = minutes % 60
+
+  const hoursText = hours > 0 ? hours + ' hr' + (hours > 1 ? 's' : '') : ''
+  const minutesText = remainingMinutes > 0 ? remainingMinutes + ' min' + (remainingMinutes > 1 ? 's' : '') : ''
+
+  if (hoursText && minutesText) {
+    return hoursText + ' ' + minutesText
+  } else {
+    return hoursText || minutesText
+  }
+}
 
 const AgentDashboard = () => {
   const auth = useAuth()
-
+  const [flights, setFlights] = useState(null)
   return (
     <Grid container spacing={6}>
       <Grid item xs={12} md={12}>
-        <HorizontalForm />
+        <Card sx={{ p: 5 }}>
+          <HorizontalForm setFlights={setFlights} />
+        </Card>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <Congratulations user={auth?.user} />
-      </Grid>
+
+      {flights &&
+        flights.itineraryGroups[0].itineraries.map(itinerary => {
+          let legDescriptions = flights.itineraryGroups[0].groupDescription.legDescriptions
+          let destination = airports.filter(a => a.VENDOR_CODE === legDescriptions[0].arrivalLocation)
+          let origin = airports.filter(a => a.VENDOR_CODE === legDescriptions[0].departureLocation)
+          let legs = itinerary.legs
+          let basePrice = `${itinerary.pricingInformation[0].fare.totalFare.baseFareCurrency} ${itinerary.pricingInformation[0].fare.totalFare.baseFareAmount}`
+          let flight_stops,
+            totalMinutes,
+            flightSchedules = []
+          // for oneway trip
+          legs.forEach(leg => {
+            let legRef = leg.ref
+            let legDescs = flights.legDescs.filter(l => l.id === legRef)
+            console.log(legDescs)
+            let schedules = legDescs[0].schedules
+            schedules.forEach(sc => {
+              flights.scheduleDescs.filter(sD => {
+                if (sD.id === sc.ref) {
+                  if (sc.departureDateAdjustment) {
+                    sD = { ...sD, day_adjustment: sc.departureDateAdjustment }
+                  }
+                  flightSchedules.push(sD)
+                }
+              })
+            })
+            totalMinutes = legDescs[0].elapsedTime
+            flight_stops = schedules.length - 1
+          })
+          let data = {
+            basePrice,
+            destination,
+            origin,
+            total_time: convertMinutesToHoursAndMinutes(totalMinutes),
+            flight_stops,
+            flightSchedules
+          }
+          return (
+            <Grid item xs={12} md={12}>
+              <Congratulations flight={data} />
+            </Grid>
+          )
+        })}
     </Grid>
   )
 }
---
+++ src/views/pages/agent/bookingForm/from.js
@@ -0,0 +1,265 @@
+import React, { useState } from 'react'
+import { FormControl, InputLabel, Select, MenuItem, Button, TextField, Grid, IconButton } from '@mui/material'
+import Icon from 'src/@core/components/icon'
+import AddIcon from '@mui/icons-material/Add'
+import RemoveIcon from '@mui/icons-material/Remove'
+import toast from 'react-hot-toast'
+import { airports } from './airports'
+import axios from 'axios'
+import Congratulations from '../../../agentDashboard/Congratulations'
+
+const BookingForm = ({ setFlights }) => {
+  const today = new Date()
+  const defaultDate = today.toISOString().split('T')[0]
+  const [formData, setFormData] = useState({
+    trip_type: 'oneway', //oneway, round
+    origin: '',
+    destination: '',
+    depart_date: defaultDate,
+    return_date: defaultDate,
+    passengers: {
+      adults: 0,
+      children: 0,
+      infants: 0
+    }
+  })
+  const [dropdownVisible, setDropdownVisible] = useState(false)
+
+  const handleChange = e => {
+    const { name, value } = e.target
+    if (name === 'passengerType') {
+      const passengerType = value
+      setFormData({
+        ...formData,
+        passengerType
+      })
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value
+      })
+    }
+  }
+
+  const handlePassengerChange = (name, increment) => {
+    setFormData(prevData => ({
+      ...prevData,
+      passengers: {
+        ...prevData.passengers,
+        [name]: Math.max(prevData.passengers[name] + increment, 0)
+      }
+    }))
+  }
+
+  const toggleDropdown = () => {
+    setDropdownVisible(prevVisible => !prevVisible)
+  }
+
+  const handleSwapLocations = () => {
+    setFormData({
+      ...formData,
+      origin: formData.destination,
+      destination: formData.origin
+    })
+  }
+
+  const isFormValid = data => {
+    return (
+      data.origin.trim() !== '' &&
+      data.destination.trim() !== '' &&
+      Object.values(data.passengers).reduce((acc, count) => acc + count, 0) > 0
+    )
+  }
+
+  const handleSubmit = async data => {
+    if (isFormValid(data)) {
+      console.log('Form is valid, submitting data:', data)
+      toast.success('Form is valid, submitting data')
+      // call to api.
+      try {
+        let response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/sabre/flights`, data)
+        let responseData = response.data
+        toast.success(responseData.message)
+        if (responseData.data.groupedItineraryResponse.statistics.itineraryCount === 0) {
+          console.log('not found')
+          toast.error('No flights Found')
+        } else {
+          setFlights(responseData.data.groupedItineraryResponse)
+        }
+        console.log(responseData.data.groupedItineraryResponse.statistics.itineraryCount)
+      } catch (error) {
+        console.log(error)
+        toast.error(error.response ? error.response.data.errors.map(err => <li>{err.msg}</li>) : error)
+      }
+
+      //call api end here
+      toggleDropdown()
+    } else {
+      console.log('Form is not valid. Please fill in all required fields.')
+      toast.error('Form is not valid. Please fill in all required fields.')
+      if (data.origin.trim() !== '' && data.destination.trim() !== '') {
+        toggleDropdown()
+      }
+    }
+  }
+
+  const hasPassengers = Object.values(formData.passengers).some(count => count > 0)
+
+  return (
+    <form>
+      <Grid container spacing={5}>
+        <Grid sx={{ display: 'flex', gap: 2 }} item xs={6} md={6}>
+          <FormControl fullWidth>
+            <InputLabel for='from-label'>From</InputLabel>
+            <Select labelId='from-label' label='From' name='origin' value={formData.origin} onChange={handleChange}>
+              {airports.slice(0, 200).map(airport => (
+                <MenuItem value={airport.VENDOR_CODE}>
+                  {airport.POI_NAME}, {airport.COUNTRY_CODE} ({airport.VENDOR_CODE})
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <IconButton onClick={handleSwapLocations}>
+            <Icon icon='material-symbols:swap-horiz' />
+          </IconButton>
+          <FormControl fullWidth>
+            <InputLabel for='to-label'>To</InputLabel>
+            <Select
+              labelId='to-label'
+              label='To'
+              name='destination'
+              value={formData.destination}
+              onChange={handleChange}
+            >
+              {airports.slice(0, 200).map(airport => (
+                <MenuItem value={airport.VENDOR_CODE}>
+                  {airport.POI_NAME}, {airport.COUNTRY_CODE} ({airport.VENDOR_CODE})
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <TextField
+            name='depart_date'
+            label='Depart At'
+            type='date'
+            fullWidth
+            value={formData.depart_date}
+            onChange={handleChange}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Grid>
+        {/* <Grid item xs={6} md={3}>
+          <TextField
+            name='return_date'
+            label='Return'
+            type='date'
+            fullWidth
+            value={formData.return_date}
+            onChange={handleChange}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Grid> */}
+        <Grid item xs={6} md={4}>
+          <Button style={{ marginTop: '10px', width: 200 }} variant='outlined' onClick={toggleDropdown}>
+            Select Passengers
+          </Button>
+          {dropdownVisible && (
+            <div
+              style={{
+                borderRadius: '15px',
+                marginTop: 20,
+                paddingTop: 30,
+                padding: '30px',
+                boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+                position: 'relative'
+              }}
+            >
+              <IconButton
+                size='small'
+                onClick={toggleDropdown}
+                sx={{ color: 'text.primary', position: 'absolute', right: 2, top: 2 }}
+              >
+                <Icon icon='mdi:close' fontSize={20} />
+              </IconButton>
+
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <p>Adults</p>
+                <div>
+                  <IconButton color='warning' onClick={() => handlePassengerChange('adults', 1)}>
+                    <AddIcon />
+                  </IconButton>
+                  {formData.passengers.adults}
+                  <IconButton color='warning' onClick={() => handlePassengerChange('adults', -1)}>
+                    <RemoveIcon />
+                  </IconButton>
+                </div>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <span>Children</span>
+                <div>
+                  <IconButton color='warning' onClick={() => handlePassengerChange('children', 1)}>
+                    <AddIcon />
+                  </IconButton>
+                  {formData.passengers.children}
+                  <IconButton color='warning' onClick={() => handlePassengerChange('children', -1)}>
+                    <RemoveIcon />
+                  </IconButton>
+                </div>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <span>Infants</span>
+                <div>
+                  <IconButton color='warning' onClick={() => handlePassengerChange('infants', 1)}>
+                    <AddIcon />
+                  </IconButton>
+                  {formData.passengers.infants}
+                  <IconButton color='warning' onClick={() => handlePassengerChange('infants', -1)}>
+                    <RemoveIcon />
+                  </IconButton>
+                </div>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+                <Button onClick={toggleDropdown} variant='contained' color='primary'>
+                  Ok
+                </Button>
+              </div>
+            </div>
+          )}
+        </Grid>
+        <Grid item xs={6} md={4}>
+          {hasPassengers && (
+            <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <p style={{ fontSize: 20 }}>
+                <strong>Adults:</strong> {formData.passengers?.adults}
+              </p>
+              <p style={{ fontSize: 20 }}>
+                <strong>Childrens:</strong> {formData.passengers?.children}
+              </p>
+              <p style={{ fontSize: 20 }}>
+                <strong>Infants:</strong> {formData.passengers?.infants}
+              </p>
+            </div>
+          )}
+        </Grid>
+        <Grid item xs={6} md={4}>
+          <Button
+            variant='contained'
+            color='primary'
+            style={{ marginTop: '10px', float: 'right', width: 180 }}
+            onClick={() => handleSubmit(formData)} // Replace with your submit logic
+          >
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  )
+}
+
+export default BookingForm
